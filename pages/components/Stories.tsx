b1085098303208e84e2f099acceeab2a5f7d4090
eitse <ewumiitse@gmail.com>
2023-01-18 12:24:01 +0100

    first commit

---
+++ pages/components/Stories.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import StoryCard from "./StoryCard";
+
+type Props = {};
+
+const stories = [
+  {
+    name: "Young Hit Eitse",
+    src: "/images/eitsepix3.jpeg",
+    profile: "/images/eitsepix2.jpeg",
+  },
+  {
+    name: "Jeff Bezoz",
+    src: "https://links.papareact.com/k2j",
+    profile: "https://links.papareact.com/f0p",
+  },
+  {
+    name: "Mark Zuckerberg",
+    src: "https://links.papareact.com/xql",
+    profile: "https://links.papareact.com/snf",
+  },
+  {
+    name: "Bill Gates",
+    src: "https://links.papareact.com/4u4",
+    profile: "https://links.papareact.com/zvy",
+  },
+  {
+    name: "Elon Musk",
+    src: "https://links.papareact.com/4zn",
+    profile: "https://links.papareact.com/kxk",
+  },
+];
+
+function Stories({}: Props) {
+  return (
+    <div className="flex justify-center space-x-3">
+      {stories.map((story) => {
+        return (
+          <StoryCard
+            key={story.src}
+            name={story.name}
+            src={story.src}
+            profile={story.profile}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Stories;
